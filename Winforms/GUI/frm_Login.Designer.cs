ba17c8d3dbeaf9cda516b0e69e1a50117ee3e5d2
Phu1237 <phu1237@gmail.com>
2021-06-03 17:29:03 +0700

    upload missing file

--- Winforms/GUI/frm_Login.Designer.cs
+++ Winforms/GUI/frm_Login.Designer.cs
@@ -33,23 +33,27 @@ namespace Winforms.GUI
             this.txt_Password = new System.Windows.Forms.TextBox();
             this.btn_Login = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.iconPictureBox1 = new FontAwesome.Sharp.IconPictureBox();
+            this.ico_Email = new FontAwesome.Sharp.IconPictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ico_Email)).BeginInit();
             this.SuspendLayout();
             // 
             // txt_Email
             // 
             this.txt_Email.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_Email.Location = new System.Drawing.Point(92, 160);
+            this.txt_Email.Location = new System.Drawing.Point(130, 160);
             this.txt_Email.Name = "txt_Email";
-            this.txt_Email.Size = new System.Drawing.Size(357, 30);
+            this.txt_Email.Size = new System.Drawing.Size(319, 30);
             this.txt_Email.TabIndex = 0;
             this.txt_Email.Text = "example@example.org";
             // 
             // txt_Password
             // 
             this.txt_Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_Password.Location = new System.Drawing.Point(92, 234);
+            this.txt_Password.Location = new System.Drawing.Point(130, 234);
             this.txt_Password.Name = "txt_Password";
-            this.txt_Password.Size = new System.Drawing.Size(357, 30);
+            this.txt_Password.Size = new System.Drawing.Size(319, 30);
             this.txt_Password.TabIndex = 1;
             this.txt_Password.Text = "password";
             this.txt_Password.UseSystemPasswordChar = true;
@@ -57,11 +61,12 @@ namespace Winforms.GUI
             // btn_Login
             // 
             this.btn_Login.BackColor = System.Drawing.Color.Goldenrod;
+            this.btn_Login.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_Login.Location = new System.Drawing.Point(92, 304);
             this.btn_Login.Name = "btn_Login";
             this.btn_Login.Size = new System.Drawing.Size(357, 37);
             this.btn_Login.TabIndex = 2;
-            this.btn_Login.Text = "Login";
+            this.btn_Login.Text = "Đăng nhập";
             this.btn_Login.UseVisualStyleBackColor = false;
             this.btn_Login.Click += new System.EventHandler(this.btn_Login_Click);
             // 
@@ -76,6 +81,32 @@ namespace Winforms.GUI
             this.label1.TabIndex = 6;
             this.label1.Text = "ABC Restaurant";
             // 
+            // iconPictureBox1
+            // 
+            this.iconPictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.iconPictureBox1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.Key;
+            this.iconPictureBox1.IconColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconPictureBox1.Location = new System.Drawing.Point(92, 236);
+            this.iconPictureBox1.Name = "iconPictureBox1";
+            this.iconPictureBox1.Size = new System.Drawing.Size(32, 32);
+            this.iconPictureBox1.TabIndex = 8;
+            this.iconPictureBox1.TabStop = false;
+            // 
+            // ico_Email
+            // 
+            this.ico_Email.BackColor = System.Drawing.Color.Transparent;
+            this.ico_Email.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.ico_Email.IconChar = FontAwesome.Sharp.IconChar.Envelope;
+            this.ico_Email.IconColor = System.Drawing.SystemColors.ControlText;
+            this.ico_Email.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.ico_Email.Location = new System.Drawing.Point(92, 162);
+            this.ico_Email.Name = "ico_Email";
+            this.ico_Email.Size = new System.Drawing.Size(32, 32);
+            this.ico_Email.TabIndex = 9;
+            this.ico_Email.TabStop = false;
+            // 
             // frm_Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -83,6 +114,8 @@ namespace Winforms.GUI
             this.BackgroundImage = global::Winforms.Properties.Resources.background;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ico_Email);
+            this.Controls.Add(this.iconPictureBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btn_Login);
             this.Controls.Add(this.txt_Password);
@@ -90,6 +123,8 @@ namespace Winforms.GUI
             this.Name = "frm_Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ico_Email)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -101,5 +136,7 @@ namespace Winforms.GUI
         private System.Windows.Forms.TextBox txt_Password;
         private System.Windows.Forms.Button btn_Login;
         private System.Windows.Forms.Label label1;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox1;
+        private FontAwesome.Sharp.IconPictureBox ico_Email;
     }
 }
