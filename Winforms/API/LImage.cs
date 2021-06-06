43d4365ecac408fbb0f3bc3a293088708aaa2e1b
Phu1237 <phu1237@gmail.com>
2021-06-07 02:18:28 +0700

    add class to Load Image from url

---
+++ Winforms/API/LImage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Winforms.API
+{
+    public class LImage
+    {
+        public Image Load(string url)
+        {
+            System.Net.WebRequest request =
+                System.Net.WebRequest.Create(url);
+
+            System.Net.WebResponse response = request.GetResponse();
+            System.IO.Stream responseStream =
+                response.GetResponseStream();
+
+            Bitmap bmp = new Bitmap(responseStream);
+
+            responseStream.Dispose();
+
+            return bmp;
+        }
+    }
+}
